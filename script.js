3fb59cd1827236b37d0241299fdda13f117f2dc7
Divya <divya.arigay@gmail.com>
2022-01-09 15:48:46 -0700

    concat

--- script.js
+++ script.js
@@ -4,26 +4,28 @@ var generateBtn = document.querySelector("#generate");
 
 //various arrays
 
-//var number =  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-//var specialChar = ["!", "@","#", "%", "&","^", ",", "$", "(", ")", "*", "+", "-", ".", "/","[", "]", "\", "<", ">", "?",];
-//var alphaLower =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number =  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "@","#", "%", "&","^", ",", "$", "(", ")", "*", "+", "-", ".", "/","[", "]", "<", ">", "?"];
+var alphaLower =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
 // Variable Declaration 
 var confirmLength = "";
-var confirmSpecialCharacter;
-var confirmNumericCharacter;
-var confirmUpperCase;
-var confirmLowerCase;
 
 //function generatePassword()
 function generatePassword() {
 
   // prompt to confirm length of the password
   var confirmLength = (prompt("How many characters would you like in your password ?"));
+     
+  
+ if(!confirmLength){
+  window.confirm("Thank you ! Come back later");
+   return;
+ }
 
-  // loop for the answers outside limits
+   // loop for the answers outside limits
   if (confirmLength < 8 || confirmLength > 128) {
     alert("password length must be minimum of 8 characters and maximum of 128 charaters");
     var confirmLength = (prompt("How many characters would you like in your password ?"));
@@ -31,10 +33,7 @@ function generatePassword() {
   else if (confirmLength >= 8 || confirmLength <= 128) {
     window.alert("Your password will have " + confirmLength + " characters.");
   }
-  else {
-    window.confirm("Thank you ! Come back later");
-  }
-
+  
   // Determine parameters of password 
   var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
   var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");
@@ -55,8 +54,27 @@ function generatePassword() {
 
   // for loop for password generation using math.random()
   var password = '';
-  var passChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~`! @#$%^&*()_-+={[}]|\:;<,>.?/0123456789';
+  var passChars = [];
 
+  // building the bank of characters based on user defined
+  if(confirmSpecialCharacter){
+    passChars = passChars.concat(specialChar);
+  }
+
+  if(confirmNumericCharacter){
+    passChars = passChars.concat(number);
+  }
+
+  if(confirmLowerCase){
+    passChars = passChars.concat(alphaLower);
+  }
+
+  if(confirmUpperCase){
+    passChars = passChars.concat(alphaUpper);
+  }
+  
+
+ // for loop for generating the password
   for (var i = 0; i < confirmLength; i++) {
 
     var password = password + passChars[Math.floor(Math.random() * passChars.length)];
